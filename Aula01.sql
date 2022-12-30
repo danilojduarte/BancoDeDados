e912242173c904a5748904da1639148fff100a18
DaniloDuarte <danilojoseduarte@gmail.com>
2022-12-30 01:23:49 -0300

    Aula iniciada

--- Aula01.sql
+++ Aula01.sql
@@ -1,14 +1,6 @@
-create database cadastro
-default character set utf8
-default collate utf8_general_ci;
+insert into pessoas
+(nome, nascimento, sexo, peso, altura, nacionalidade)
+values
+('Godofredo', '1984-01-02', 'M', '78.5', '1.83', 'Brasil');
 
-create table pessoas (
-			id int NOT NULL auto_increment,
-            nome varchar(30) NOT NULL,
-            nascimento date,
-            sexo enum ('M', 'F'),
-            peso decimal (5,2),
-            altura decimal (3,2),
-            nacionalidade varchar (20) default 'Brasil',
-            primary key (id)
-        ) default charset = utf8;
+select * from pessoas;
