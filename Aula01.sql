64c08b4c3889c51681e7ed0bae457e81905a81d7
DaniloDuarte <danilojoseduarte@gmail.com>
2022-12-31 01:14:06 -0300

    Aula 5 – Inserindo Dados na Tabela (INSERT INTO) Finalizada

--- Aula01.sql
+++ Aula01.sql
@@ -1,6 +1,8 @@
 insert into pessoas
 (nome, nascimento, sexo, peso, altura, nacionalidade)
 values
-('Godofredo', '1984-01-02', 'M', '78.5', '1.83', 'Brasil');
+(DEFAULT, 'Danilo', '1984-10-04', 'M', '74,0', '1.69', 'Brasil'),
+(default, 'Isis', '1986-02-24', 'F', '76,01', '1.57', 'Brasil'),
+(default, 'Isabela', '2017-07-03', 'F', '20.8', '1.00', 'Brasil');
 
 select * from pessoas;
