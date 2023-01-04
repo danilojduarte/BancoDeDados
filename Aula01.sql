942522dcab6295ef51a4b63de7c56d3be785354d
DaniloDuarte <danilojoseduarte@gmail.com>
2023-01-04 11:25:41 -0300

    Aula finalizada para geração de tabelas

--- Aula01.sql
+++ Aula01.sql
@@ -1,6 +1,9 @@
 describe pessoas;
 
 alter table pessoas
+rename to gafanhotos;
+
+alter table pessoas
 add column profissao varchar(10) after nome;
 
 alter table pessoas
@@ -16,5 +19,13 @@ select * from pessoas;
 
 alter table pessoas
 drop column profissao;
+
+create table if not exists cursos (
+nome varchar(30) not null  unique,
+descricao text,
+carga int unsigned,
+totalaulas int unsigned,
+ano year default '2016'
+) default charset = utf8;
 
 
