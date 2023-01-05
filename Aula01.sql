10bdbf96caaaaaf2f9d476a8699e3fb903467039
DaniloDuarte <danilojoseduarte@gmail.com>
2023-01-05 12:04:03 -0300

    Aula Finalizada

--- Aula01.sql
+++ Aula01.sql
@@ -20,5 +20,11 @@ update cursos
 set nome = 'PHP', ano = '2015'
 where idcurso = '4';
 
+update cursos
+set nome = 'Java', ano = '2015', carga = '40'
+where idcurso = '5'
+limit 1;
 
+delete from cursos
+where idcurso = '8';
 
