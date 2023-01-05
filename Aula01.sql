621054d4883de30ae815b49f9481ffd57210c9ce
DaniloDuarte <danilojoseduarte@gmail.com>
2023-01-05 01:46:18 -0300

    Aula iniciada para edição de linha

--- Aula01.sql
+++ Aula01.sql
@@ -1,31 +1,24 @@
-describe pessoas;
+insert into cursos values
+('1', 'HTML4',	'Curso de HTML5', '40', '37', '2014'),
+('2', 'Algoritmos', 'Lógica de Programação', '20', '15', '2014'),
+('3', 'Photoshop', 'Dicas de Photoshop CC', '10', '8', '2014'),
+('4', 'PGP', 'Curso de PHP para iniciantes', '40', '20', '2010'),
+('5', 'Jarva', 'Introdução à Linguagem Java','10', '29', '2000'),
+('6', 'MySQL', 'Bancos de Dados MySQL', '30', '15', '2016'),
+('7', 'Word', 'Curso Completo de Word', '40', '30', '2016'),
+('8', 'Sapateado', 'Danças Ritimicas', '40', '30', '2018'),
+('9', 'Cozinha Árabe', 'Aprenda a fazer Kibe', '40', '30', '2018'),
+('10', 'Youtuber', 'Gerar polemica e ganhar inscritos', '5', '2', '2018');
 
-alter table pessoas
-rename to gafanhotos;
+select * from cursos;
 
-alter table pessoas
-add column profissao varchar(10) after nome;
+update cursos
+set nome = 'HTML5'
+where idcurso = '1';
 
-alter table pessoas
-add column codigo int first;
+update cursos
+set nome = 'PHP', ano = '2015'
+where idcurso = '4';
 
-alter table pessoas
-modify column profissao varchar(20) not null default '';
-
-alter table pessoas
-change column profissao prof varchar(20);
-
-select * from pessoas;
-
-alter table pessoas
-drop column profissao;
-
-create table if not exists cursos (
-nome varchar(30) not null  unique,
-descricao text,
-carga int unsigned,
-totalaulas int unsigned,
-ano year default '2016'
-) default charset = utf8;
 
 
